9be3ad5c318df1b03e5f3fd2290b9ef2f7837083
MitchJo <mitchell@exploreembedded.com>
2018-08-15 15:45:53 +0530

    Added chat and channel vc

---
+++ smack/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by MacbookPro on 15/08/18.
+//  Copyright © 2018 MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+   
+
+}
---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,33 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by MacbookPro on 15/08/18.
+//  Copyright © 2018 MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
