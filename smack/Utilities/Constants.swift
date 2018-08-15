d9753759c280928864185ff6616e23a39b95f7ec
MitchJo <mitchell@exploreembedded.com>
2018-08-15 17:11:09 +0530

    Added LoginVC

---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by MacbookPro on 15/08/18.
+//  Copyright © 2018 MacbookPro. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
