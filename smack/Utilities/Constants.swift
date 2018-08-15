3c1191d4f3c35566b0a8519a766478c5551e6ab4
MitchJo <mitchell@exploreembedded.com>
2018-08-15 23:25:24 +0530

    User Avatar

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -22,6 +22,7 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
+let TO_AVATAR_PICKER = "toAvatarPicker"
 let UNWIND = "unwindToChannel"
 
 //UserDefaults
---
+++ smack/View/AvatarCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionViewCell.swift
+//  smack
+//
+//  Created by MacbookPro on 15/08/18.
+//  Copyright © 2018 MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView(){
+        self.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
