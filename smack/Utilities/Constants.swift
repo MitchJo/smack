1175e62bcc3ea3d18cb6f863dfbe48784e1e6664
MitchJo <mitchell@exploreembedded.com>
2018-08-15 21:39:27 +0530

    Added API feature

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Url constants
+
+let BASE_URL = "https://chattychatmit.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)/v1/account/register"
+let URL_ACCOUNT = "\(BASE_URL)"
+let URL_LOGIN = "\(BASE_URL)"
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//UserDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
